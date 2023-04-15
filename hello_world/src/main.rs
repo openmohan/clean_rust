8b6e13d9957a2466a746c9dced3e7de2d74e9499
Mohan Prasath Sekar <openmohan@gmail.com>
2023-04-15 15:11:55 +0530

    hello-world: play with ferris

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
     println!("Hello, world!");
+    let input = String::from("hello world");
+    let max_width = usize::from_be(5);
+    let mut writer = BufWriter::new(stdout().lock());
+    say(input.as_bytes(), max_width,&mut writer).unwrap();
 }
