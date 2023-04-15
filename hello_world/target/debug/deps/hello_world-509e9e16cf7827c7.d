57dc36c7d6b6fc37497680a90bac24251ffab386
Mohan Prasath Sekar <openmohan@gmail.com>
2023-04-15 14:41:18 +0530

    init commit - hello world

---
+++ hello_world/target/debug/deps/hello_world-509e9e16cf7827c7.d
@@ -0,0 +1,5 @@
+/home/mohan/rustWorkspace/clean_rust/hello_world/target/debug/deps/hello_world-509e9e16cf7827c7.rmeta: src/main.rs
+
+/home/mohan/rustWorkspace/clean_rust/hello_world/target/debug/deps/hello_world-509e9e16cf7827c7.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/mohan/rustWorkspace/clean_rust/hello_world/target/debug/hello_world: /home/mohan/rustWorkspace/clean_rust/hello_world/src/main.rs
