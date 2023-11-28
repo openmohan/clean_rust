1ee021c02b268078f546f2fbd732c21e2633aa3c
Mohan Prasath S <openmohan@gmail.com>
2023-11-28 22:18:42 +0530

    string format

---
+++ formatAndCast/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    let mut x = 2;
+    x = 10;
+    println!("tests{}",x);
+    let x = "test";
+    println!("{}tests{}",x,x);
+    let y:i32 = 78;
+    println!("{}sdfasf",y)
+}
