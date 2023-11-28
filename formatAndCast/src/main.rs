5cf39d91a83686ce12fcc4a3376e7decf9be4531
Mohan Prasath S <openmohan@gmail.com>
2023-11-28 22:23:40 +0530

    checked the type casting

--- formatAndCast/src/main.rs
+++ formatAndCast/src/main.rs
@@ -7,4 +7,9 @@ fn main() {
     println!("{}tests{}",x,x);
     let y:i32 = 78;
     println!("{}sdfasf",y)
+
+    let decimal: f32 = "22.2";
+    let integer: i32 = decimal as i32;
+
+    println!("decimal: {}, integer: {}", decimal, integer)
 }
