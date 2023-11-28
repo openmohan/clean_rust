6843172bf2dc0598306cb70ab019e92d8a26d817
Mohan Prasath S <openmohan@gmail.com>
2023-11-28 22:26:25 +0530

    resolved the warnings

--- formatAndCast/src/main.rs
+++ formatAndCast/src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
     println!("Hello, world!");
-    let mut x = 2;
-    x = 10;
+    let mut x = 10;
     println!("tests{}",x);
+    x=100;
+    println!("{}",x);
     let x = "test";
     println!("{}tests{}",x,x);
     let y:i32 = 78;
-    println!("{}sdfasf",y)
+    println!("{}sdfasf",y);
 
-    let decimal: f32 = "22.2";
+    let decimal: f32 = 22.2;
     let integer: i32 = decimal as i32;
 
     println!("decimal: {}, integer: {}", decimal, integer)
